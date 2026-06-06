14c8598104fda1ff5252f3b176afce48cae9cb69
msnigatsu <msnigatsu@gmail.com>
2026-06-06 18:38:53 +0800

    更改目錄結構 並修正路由

--- server.js
+++ server.js
@@ -1,85 +1,82 @@
 const http = require("http");
-const { v4: uuidv4 } = require("uuid"); // 舊版 CommonJS 寫法
-const errorHandle = require("./errorHandle");
-const todos = [{ title: "今天要刷牙", id: uuidv4() }];
+const header = require("./header");
+const successHandler = require("./successHandler");
+const errorHandler = require("./errorHandler");
+const { v4: uuidv4 } = require("uuid");
+const todos = [{ title: "買牛奶", id: uuidv4() }];
 function requestListener(req, res) {
   let body = "";
   req.on("data", (chunk) => {
     body += chunk;
   });
-  const headers = {
-    "Access-Control-Allow-Headers":
-      "Content-Type, Authorization, Content-Length, X-Requested-With",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
-    "Content-Type": "application/json",
-  };
-  if (req.url === "/todos" && req.method === "GET") {
-    res.writeHead(200, headers);
-    res.write(JSON.stringify({ status: "success", data: todos }));
-    res.end();
-  } else if (req.url === "/todos" && req.method === "POST") {
-    req.on("end", () => {
-      try {
-        const { title } = JSON.parse(body);
-        if (title !== undefined) {
-          const todo = { title, id: uuidv4() };
-          todos.push(todo);
-          res.writeHead(200, headers);
-          res.write(JSON.stringify({ status: "success", data: todos }));
-          res.end();
-        } else {
-          errorHandle(res);
-        }
-      } catch (e) {
-        errorHandle(res);
-      }
-    });
-  } else if (req.url === "/todos" && req.method === "DELETE") {
+  if (req.method === "OPTIONS") {
+    res.writeHead(200, header);
+    return res.end();
+  } else if (req.url === "/todos" && req.method === "GET") {
+    return successHandler(res, 200, todos);
+  } else if (req.url === "/todos/" && req.method === "DELETE") {
     todos.length = 0;
-    res.writeHead(200, headers);
-    res.write(JSON.stringify({ status: "success", data: todos }));
-    res.end();
-  } else if (req.url.startsWith("/todos/") && req.method === "DELETE") {
-    //.pop()會刪除陣列最後一筆資料並將該筆刪除資料回傳，所以id可以拿到"/todos/"後面的uuid
-    const id = req.url.split("/").pop();
+    return successHandler(res, 200, todos);
+  } else if (req.url === "/todos/" && req.method === "POST") {
+    req.on("end", () => {
+      let data;
+      try {
+        data = JSON.parse(body);
+      } catch (e) {
+        return errorHandler(res, 400, "JSON解析失敗");
+      }
+      const { title } = data;
+      if (!title || title.toString().trim() === "") {
+        return errorHandler(res, 400, "資料格式錯誤 或 輸入為空白");
+      }
+      todos.push({
+        title: title.toString().trim(),
+        id: uuidv4(),
+      });
+      return successHandler(res, 201, todos);
+    });
+  } else if (req.url.startsWith("/todos") && req.method === "DELETE") {
+    const urlELements = req.url.split("/");
+    if (urlELements.length !== 3) {
+      return errorHandler(res, 404, "無此路由");
+    }
+    const id = urlELements.pop();
     const index = todos.findIndex((e) => e.id === id);
     if (index === -1) {
-      errorHandle(res);
-    } else {
-      todos.splice(index, 1);
-      res.writeHead(200, headers);
-      res.write(JSON.stringify({ status: "success", data: todos }));
-      res.end();
+      return errorHandler(res, 400, "id錯誤");
     }
-  } else if (req.url.startsWith("/todos/") && req.method === "PATCH") {
+    todos.splice(index, 1);
+    return successHandler(res, 200, todos);
+  } else if (req.url.startsWith("/todos") && req.method === "PATCH") {
+    let data;
     req.on("end", () => {
       try {
-        const { title } = JSON.parse(body);
-        const id = req.url.split("/").pop();
-        const index = todos.findIndex((e) => e.id === id);
-        if (title && index !== -1) {
-          todos[index].title = title;
-          res.writeHead(200, headers);
-          res.write(JSON.stringify({ status: "success", data: todos }));
-          res.end();
-        } else {
-          errorHandle(res);
-        }
+        data = JSON.parse(body);
       } catch (e) {
-        errorHandle(res);
+        return errorHandler(res, 400, "JSON解析失敗");
       }
+      const { title } = data;
+      if (!title || title.toString().trim() === "") {
+        return errorHandler(res, 400, "資料格式錯誤 或 輸入為空白");
+      }
+
+      const urlELements = req.url.split("/");
+      if (urlELements.length !== 3) {
+        return errorHandler(res, 404, "無此路由");
+      }
+      const id = urlELements.pop();
+      const index = todos.findIndex((e) => e.id === id);
+      if (index === -1) {
+        return errorHandler(res, 400, "id錯誤");
+      }
+      todos[index].title = title.toString().trim();
+      return successHandler(res, 200, todos);
     });
-  } else if (req.method === "OPTIONS") {
-    res.writeHead(200, headers);
-    res.end();
   } else {
-    res.writeHead(404, headers);
-    res.write(JSON.stringify({ status: "fail", message: "無此網站路由" }));
-    res.end();
+    return errorHandler(res, 404, "無此路由");
   }
 }
 const server = http.createServer(requestListener);
 server.listen(3005, () => {
-  console.log("server is running");
+  console.log("server is running.");
 });
